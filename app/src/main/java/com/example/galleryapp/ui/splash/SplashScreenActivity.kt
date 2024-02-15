2dbc1b7f3d227d3d3076e5c964890a65e5f26937
abhaysaini <abhaysaini1974@gmail.com>
2024-02-15 11:15:50 +0530

    Android 12-14 Permission bug resolved

--- app/src/main/java/com/example/galleryapp/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/galleryapp/ui/splash/SplashScreenActivity.kt
@@ -1,34 +1,52 @@
 package com.example.galleryapp.ui.splash
 
 import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.galleryapp.R
+import com.example.galleryapp.databinding.ActivitySplashScreenBinding
 import com.example.galleryapp.ui.albums.AlbumsActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    lateinit var binding:ActivitySplashScreenBinding
+
     private val splashTime: Long = 2000
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         startSplashTimer()
     }
 
+
     private fun startSplashTimer() {
         CoroutineScope(Dispatchers.Main).launch {
+            askPermission()
+            yield()
             delay(splashTime)
             navigateToMainActivity()
         }
@@ -37,41 +55,43 @@ class SplashScreenActivity : AppCompatActivity() {
     private fun navigateToMainActivity() {
         val intent = Intent(this, AlbumsActivity::class.java)
         startActivity(intent)
-        askPermission()
         finish()
     }
 
+
     private fun askPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                Log.i("abhay", "Permission Asked")
-                ActivityCompat.requestPermissions(
-                    this, arrayOf(
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                    ), RESULT_CODE_GALLERY
-                )
-                requestGalleryPermission()
-                Log.i("abhay", "Permission granted")
-            }
+        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            ContextCompat.checkSelfPermission(this, READ_MEDIA_IMAGES) == PERMISSION_GRANTED
+        ) {
+            PERMISSION_GRANTED
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE &&
+            ContextCompat.checkSelfPermission(
+                this,
+                READ_MEDIA_VISUAL_USER_SELECTED
+            ) == PERMISSION_GRANTED
+        ) {
+            PERMISSION_GRANTED
+        } else if (ContextCompat.checkSelfPermission(
+                this,
+                READ_EXTERNAL_STORAGE
+            ) == PERMISSION_GRANTED
+        ) {
+            PERMISSION_GRANTED
+        } else {
+            PackageManager.PERMISSION_DENIED
+        }
+
+        if (permission != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    READ_EXTERNAL_STORAGE,
+                    READ_MEDIA_IMAGES
+                ),
+                RESULT_CODE_GALLERY
+            )
         }
     }
-
-    private fun requestGalleryPermission() {
-        val permissionRequest: MutableList<String> = ArrayList()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.READ_MEDIA_IMAGES
-                ) == PackageManager.PERMISSION_GRANTED) {
-                permissionRequest.add(Manifest.permission.READ_MEDIA_IMAGES)
-            }
-        }
-    }
-
-
 
     companion object {
         private const val RESULT_CODE_GALLERY = 1001
