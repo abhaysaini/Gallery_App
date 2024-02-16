415d9e7f99397af1d3980d1261fb6596a0ef9fa3
abhaysaini <abhaysaini1974@gmail.com>
2024-02-16 14:24:21 +0530

    Android 12 permission && compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
-//        compose = true
+        compose = true
     }
     packaging {
         resources {
@@ -47,22 +47,24 @@ android {
 
 dependencies {
 
-    //Compose
-//    implementation("androidx.core:core-ktx:1.12.0")
-//    implementation("androidx.activity:activity-compose:1.8.2")
-//    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-//    implementation("androidx.compose.ui:ui")
-//    implementation("androidx.compose.ui:ui-graphics")
-//    implementation("androidx.compose.ui:ui-tooling-preview")
-//    implementation("androidx.compose.material3:material3")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-//    debugImplementation("androidx.compose.ui:ui-tooling")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
-//    implementation ("androidx.compose.material3:material3:$1.2.0")
+//    Compose
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material3:material3:$1.2.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.23.1")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -83,8 +85,8 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
     // viewModel
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
     //Coroutines
@@ -92,7 +94,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     //Zoom In-Out
-    implementation ("com.github.MikeOrtiz:TouchImageView:1.4.1")
+    implementation("com.github.MikeOrtiz:TouchImageView:1.4.1")
 
     implementation("com.google.code.gson:gson:2.8.8")
 
--- app/src/main/java/com/example/galleryapp/ui/splash/SpalshScreen.kt
+++ app/src/main/java/com/example/galleryapp/ui/splash/SpalshScreen.kt
@@ -1,128 +1,166 @@
-//package com.example.galleryapp.ui.splash
-//
-//import android.Manifest
-//import android.content.Intent
-//import android.content.pm.PackageManager
-//import android.os.Build
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.result.contract.ActivityResultContracts
-//import androidx.compose.foundation.Image
-//import androidx.compose.foundation.layout.Arrangement
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.layout.size
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.rememberCoroutineScope
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.layout.ContentScale
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.text.font.FontWeight
-//import androidx.compose.ui.unit.dp
-//import com.example.galleryapp.R
-//import com.example.galleryapp.ui.albums.AlbumsActivity
-//import kotlinx.coroutines.delay
-//import kotlinx.coroutines.launch
-//import androidx.compose.material3.Text
-//
-//class SplashScreen : ComponentActivity() {
-//
-//    private val splashTime: Long = 2000
-//
-//    private val requestPermissionLauncher =
-//        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-//            if (isGranted) {
-//                Log.i("abhay", "Permission granted")
-//            } else {
-//                Log.i("abhay", "Permission denied")
-//            }
-//        }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            SplashScreenContent()
-//        }
-//        startSplashTimer()
-//    }
-//
-//    @Composable
-//    private fun startSplashTimer() {
-//        val scope = rememberCoroutineScope()
-//        scope.launch {
-//            delay(splashTime)
-//            navigateToMainActivity()
-//        }
-//    }
-//
-//    private fun navigateToMainActivity() {
-//        val intent = Intent(this, AlbumsActivity::class.java)
-//        startActivity(intent)
-//        askPermission()
-//        finish()
-//    }
-//
-//    private fun askPermission() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                Log.i("abhay", "Permission Asked")
-//                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-//                requestGalleryPermission()
-//            }
-//        }
-//    }
-//
-//    private fun requestGalleryPermission() {
-//        val permissionRequest: MutableList<String> = ArrayList()
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-//            if (checkSelfPermission(Manifest.permission.READ_MEDIA_IMAGES)
-//                == PackageManager.PERMISSION_GRANTED
-//            ) {
-//                permissionRequest.add(Manifest.permission.READ_MEDIA_IMAGES)
-//            }
-//        }
-//    }
-//
-//    @Composable
-//    fun SplashScreenContent() {
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(16.dp),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            verticalArrangement = Arrangement.Center
-//        ) {
-//            Image(
-//                painter = painterResource(id = R.drawable.splash_img),
-//                contentDescription = "Splash Image",
-//                contentScale = ContentScale.Fit,
-//                modifier = Modifier
-//                    .size(200.dp)
-//                    .padding(bottom = 16.dp)
-//            )
-//            Text(
-//                text = "Gallery App",
-//                color = MaterialTheme.colorScheme.secondary,
-//                fontSize = 38.dp,
-//                fontWeight = FontWeight.Bold,
-//                style = MaterialTheme.typography.titleMedium
-//            )
-//            Text(
-//                text = "Explore your visual story",
-//                color = MaterialTheme.colorScheme.secondary,
-//                fontSize = 24.dp,
-//                fontWeight = FontWeight.Bold,
-//                modifier = Modifier.padding(top = 16.dp),
-//                style = MaterialTheme.typography.titleMedium
-//            )
-//        }
-//    }
-//}
+package com.example.galleryapp.ui.splash
+
+
+import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.galleryapp.R
+import com.example.galleryapp.ui.albums.AlbumsActivity
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreenContent()
+        }
+    }
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    @Composable
+    fun SplashScreenContent() {
+        val context = LocalContext.current
+        val permission =
+            rememberPermissionState(permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) READ_MEDIA_IMAGES else READ_EXTERNAL_STORAGE)
+        val permissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(2000)
+                    navigateToMainActivity(context)
+                }
+            } else {
+                appSettingOpen(this)
+            }
+        }
+        LaunchedEffect(key1 = permission) {
+            if(permission.hasPermission){
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(1000)
+                    navigateToMainActivity(this@SplashScreen)
+                }
+            }
+            else{
+                askPermission(permissionLauncher)
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.splash_img),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(310.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .offset(y = (-50).dp)
+
+            )
+            Text(
+                text = "Gallery App",
+                color = Color(0xFF98BFFD),
+                fontSize = 38.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Default,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .offset(y = (-16).dp)
+            )
+            Text(
+                text = "Explore your visual story",
+                color = Color(0xFF98BFFD),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Default,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+
+    private fun navigateToMainActivity(context: Context) {
+        val intent = Intent(context, AlbumsActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun appSettingOpen(context: Context) {
+        Toast.makeText(
+            context,
+            "Go to Setting and Enable All Permission",
+            Toast.LENGTH_LONG
+        ).show()
+
+        val settingIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        settingIntent.data = Uri.parse("package:${context.packageName}")
+        context.startActivity(settingIntent)
+    }
+
+    private fun askPermission(
+        permissionLauncher: ActivityResultLauncher<String>
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissionLauncher.launch(READ_MEDIA_IMAGES)
+        } else {
+            permissionLauncher.launch(READ_EXTERNAL_STORAGE)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SplashScreenContent()
+}
+
+@Composable
+fun SplashScreenContent() {
+    SplashScreenContent()
+}
